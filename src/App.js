9694224d24fb8375ae90ca05eca683be248e1a35
Alexandre GIRARD <alexandre.girard@maif.fr>
2023-11-04 17:28:28 +0100

    Create CharactersList component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './style.css';
+import { CharactersList } from './components/CharactersList';
 
 const characters = require('./data/characters.json');
 
 function App() {
+  console.log((characters));
+
   return (
     <>
       <h1>Marvel Characters</h1>
-      <ul id="characters">
-        {characters.map((character) => (
-          <li key={character.id}>{character.name}</li>
-        ))}
-      </ul>
+      <CharactersList characters={characters} />
     </>
   );
 }
