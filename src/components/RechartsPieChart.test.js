8f3a69ba9e0e89ee6b995b03b6102f4c888b8939
Alexandre GIRARD <alexandre.girard@maif.fr>
2023-11-24 10:53:15 +0100

    fix sonarqube code smell

--- src/components/RechartsPieChart.test.js
+++ src/components/RechartsPieChart.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import RechartsPieChart, { prepareData } from './RechartsPieChart';
 
 describe('RechartsPieChart', () => {
